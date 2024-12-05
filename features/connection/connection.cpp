096ff2ddd0fb5e6a84fb1d7ccbb4c4e3aee0a5d4
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-05 23:19:41 +0900

    update: changed i2c addr for right node

--- features/connection/connection.cpp
+++ features/connection/connection.cpp
@@ -34,7 +34,7 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event)
 
 // I2C connection
 
-const uint8_t I2C_SLAVE_ADDRESS = 0x10;
+const uint8_t I2C_SLAVE_ADDRESS = 0x20;
 
 const uint I2C_SDA_PIN = 16;
 const uint I2C_SCL = 17;
